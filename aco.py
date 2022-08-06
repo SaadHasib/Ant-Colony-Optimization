1519ba4a095dff9f6df7a0e63bf8d2359d5fffbb
Saad Hasib <saadhasib@outlook.com>
2022-08-06 22:05:50 +0500

    Updated implementation

--- aco.py
+++ aco.py
@@ -7,13 +7,14 @@ import seaborn as sns
 sns.set_style("darkgrid")
 sns.set_context("notebook")
 
+
 def init_ants(space, colony):
     """Initialize ant positions by selecting random nodes of the sample space.
-    
+
     Arguments:
         space {numpy.ndarray} -- The samples space
         colony {int} -- Number of ants in the colony
-    
+
     Returns:
         numpy.ndarray -- An array of indices of position in the sample space
     """
@@ -21,13 +22,14 @@ def init_ants(space, colony):
     init_pos = np.random.randint(space.shape[0], size=colony)
     return init_pos
 
+
 def find_inv_distance(space, beta):
     """Finds the inverse euclidean distances for all the nodes to all the nodes in the sample space.
-    
+
     Arguments:
         space {numpy.ndarray} -- The sample space
         beta {int/float} -- ACO prameter beta
-    
+
     Returns:
         numpy.ndarray -- A space.dimension x space.dimension array of inverse distances
     """
@@ -39,14 +41,15 @@ def find_inv_distance(space, beta):
         distances[index] = np.sqrt(((space - point) ** 2).sum(axis=1))
     # Invert the distances
     with np.errstate(all='ignore'):
-        inv_distance =  1 / distances
+        inv_distance = 1 / distances
     # Replace infinity by zero(zero division error)
     inv_distance[inv_distance == np.inf] = 0
     return inv_distance ** beta
 
+
 def ants_move(space, pos, inv_distance, pheromones, alpha, beta, del_tau):
     """Moves the ants from starting position to cover all the nodes.
-    
+
     Arguments:
         space {numpy.ndarray} -- The sample space
         pos {numpy.ndarray} -- The starting position of the ants
@@ -55,7 +58,7 @@ def ants_move(space, pos, inv_distance, pheromones, alpha, beta, del_tau):
         alpha {int/float} -- ACO parameter alpha
         beta {int/float} -- ACO parameter beta
         del_tau {int/float} -- ACO parameter delta tau
-    
+
     Returns:
         numpy.ndarray -- The indices of the path taken by the ant
     """
@@ -69,35 +72,38 @@ def ants_move(space, pos, inv_distance, pheromones, alpha, beta, del_tau):
         # For each ant
         for ant in range(pos.shape[0]):
             # Probability to travel to nodes
-            next_loc_prob = (inv_distance[pos[ant]] ** alpha + pheromones[pos[ant]] ** beta / 
-                                inv_distance[pos[ant]].sum() ** alpha + pheromones[pos[ant]].sum() ** beta)
+            next_loc_prob = ((inv_distance[pos[ant]] ** alpha) * (pheromones[pos[ant]] ** beta) /
+                             (inv_distance[pos[ant]].sum() ** alpha) * (pheromones[pos[ant]].sum() ** beta))
             # Index to maximum probability node
-            next_pos = np.argwhere(next_loc_prob == np.amax(next_loc_prob))[0][0]
+            next_pos = np.argwhere(
+                next_loc_prob == np.amax(next_loc_prob))[0][0]
             # Check if node has been already visited
             while next_pos in paths[:, ant]:
                 # Replace the probability of visited by zero
                 next_loc_prob[next_pos] = 0.0
                 # Find the maximum probability
-                next_pos = np.argwhere(next_loc_prob == np.amax(next_loc_prob))[0][0]
+                next_pos = np.argwhere(
+                    next_loc_prob == np.amax(next_loc_prob))[0][0]
             # Add node to path
             paths[node, ant] = next_pos
             # Update pheromones
             pheromones[node, next_pos] = pheromones[node, next_pos] + del_tau
     return np.swapaxes(paths, 0, 1)
 
+
 def run_aco(space, iters=50, colony=25, alpha=1.0, beta=1.0, del_tau=0.5, rho=0.5):
     """Runs the main Ant Colony Optimization algorithm.
-    
+
     Arguments:
         space {numpy.ndarray} -- The sample space
-    
+
     Keyword Arguments:
         iters {int} -- Number of iterations (default: {30})
         colony {int} -- Number of ants in the colony (default: {50})
         alpha {int/float} -- ACO parameter alpha (default: {1.0})
         beta {int/float} -- ACO parameter beta (default: {1.0})
         del_tau {int/float} -- ACO parameter delta tau (default: {0.5})
-    
+
     Returns:
         Tuple(numpy.ndarray, float) -- Indices of the minimum distance path and the minimum distance
     """
@@ -114,7 +120,8 @@ def run_aco(space, iters=50, colony=25, alpha=1.0, beta=1.0, del_tau=0.5, rho=0.
         # GEnerate initial random positions
         init_pos = init_ants(space, colony)
         # Complete a path
-        paths = ants_move(space, init_pos, inv_distance, pheromones, alpha, beta, del_tau)
+        paths = ants_move(space, init_pos, inv_distance,
+                          pheromones, alpha, beta, del_tau)
         # Evaporate the pheromones
         pheromones *= (1 - rho)
         # For each path
@@ -124,7 +131,8 @@ def run_aco(space, iters=50, colony=25, alpha=1.0, beta=1.0, del_tau=0.5, rho=0.
             # For each node from 2nd to last
             for node in range(1, path.shape[0]):
                 # Calculate distance to the last node
-                distance += np.sqrt(((space[int(path[node])] - space[int(path[node-1])]) ** 2).sum())
+                distance += np.sqrt(((space[int(path[node])] -
+                                    space[int(path[node-1])]) ** 2).sum())
             # Update minimum distance and path if less nor non existent
             if not min_distance or distance < min_distance:
                 min_distance = distance
@@ -133,11 +141,14 @@ def run_aco(space, iters=50, colony=25, alpha=1.0, beta=1.0, del_tau=0.5, rho=0.
     min_path = np.append(min_path, min_path[0])
     return (min_path, min_distance)
 
+
 def main():
     # Read CSV data file
-    post_off_data = pandas.read_csv("/home/buntu/WORK/Python/work_demo/ACO/post_offices.csv")
+    post_off_data = pandas.read_csv(
+        "/home/buntu/WORK/Python/work_demo/ACO/post_offices.csv")
     # Create array of latitude and longitude
-    space = np.array([post_off_data.values[:, 6], post_off_data.values[:, 7]], dtype=np.float_)
+    space = np.array([post_off_data.values[:, 6],
+                     post_off_data.values[:, 7]], dtype=np.float_)
     # Rearrange data
     space = np.swapaxes(space, 0, 1)
     # Run ACO and find minimum distance and path
@@ -147,13 +158,15 @@ def main():
     # Place the data nodes
     plt.scatter(space[:, 0], space[:, 1], c='r', marker='o', s=50.0)
     # Plot the minimum path
-    plt.plot(space[min_path, 0], space[min_path, 1], c='g', linewidth=1.0, linestyle='--')
+    plt.plot(space[min_path, 0], space[min_path, 1],
+             c='g', linewidth=1.0, linestyle='--')
     # Set plot properties
     plt.title("Minimum path")
     plt.xlabel("Latitude")
     plt.ylabel("Longitude")
     # Show plot
     plt.show()
+
 
 if __name__ == '__main__':
     main()
